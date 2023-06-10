cfe89ab8db8f0638d6770da8e31499d244a5cdb0
filipeveiga@FCVMac <filipe.veiga@protonmail.com>
2023-06-10 15:23:48 +0100

    Initial Commit

---
+++ secondTry/secondTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondTryApp.swift
+//  secondTry
+//
+//  Created by Filipe Veiga on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct secondTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
